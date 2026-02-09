15003b868af522e93145bf6c181ae379882b7bbf
alwar87 <alwar87@gmail.com>
2026-02-09 16:44:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,25 @@
-CopyRunprovider "google" {
-  project = "your-gcp-project-id"
-  region  = "us-central1"
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "devops-cluster"
-  location = "us-central1"
-  initial_node_count = 3
+resource "google_container_cluster" "gke" {
+  name     = "hello-gke"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster   = google_container_cluster.gke.name
+  location  = var.region
+  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
-}
-
-output "kubeconfig" {
-  value = google_container_cluster.primary.endpoint
 }
